b7987d7f818cd6a5ff4c3017ace33fc165798ad4
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2022-06-25 21:39:51 +0200

    first commit

---
+++ js/big-o.js
@@ -0,0 +1,17 @@
+function addUpTo(n) {
+  let total = 0;
+  for (let i = 0; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+
+function addUPTo2(n){
+  return   n * (n +1) /2
+}
+
+let t1 = performance.now()
+addUPTo2(10000000000);
+let t2 = performance.now()
+
+console.log(`Time Elapsed: ${(t2-t1)/1000} sec(s)`)
