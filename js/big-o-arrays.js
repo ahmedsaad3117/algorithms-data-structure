b356a18f69b37a204c115ceadb65258b4e56978c
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2022-06-25 22:31:00 +0200

    problem solving

--- js/big-o-arrays.js
+++ js/big-o-arrays.js
@@ -1,5 +1,16 @@
-let person = {
-  fristName: "john",
-  age: 15,
-  friends: ["a", "b", "c"],
-};
+let names = ["john", "adam", "sara"];
+
+names[1] //Access O(1)
+names.push('Raj') //Add At End O(1)
+names.unshift('Raj') //Add At begin O(n)
+
+names.pop() //delete from End O(1)
+names.shift() //delete from begin O(n)
+
+names.find('john') // O(n)
+
+names.sort() //O(N* log N)
+
+
+// .forEach .map .fillter .reduce/etc O(N)
+// Array .slice .splice .concat 
